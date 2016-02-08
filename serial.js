c2e300cf72996c0df10aa33edb0c5394b4fd77aa
中川修哉 <n.syuuya@gmail.com>
2016-02-08 19:13:44 +0900

    serial test

--- serial.js
+++ serial.js
@@ -1,26 +1,44 @@
 var serialPort = require("serialport")
-var sp = new serialPort.SerialPort("/dev/tty.usbmodem1421", {
-	baudrate : 9600,
-	dataBits:8,
-	parity:'none',
-	flowControl:false
-});
 
-var data = (1<<7) + 15;
+exports.Serial = function(id){
+  this.sp = new serialPort.SerialPort(id, {
+    baudrate : 9600,
+    dataBits:8,
+    parity:'none',
+    flowControl:false
+  });
 
-sp.on("open", function() {
-	console.log('open');
-	setTimeout(function(){
-		sp.write(data);		
-		console.log('sended msg');
-	}, 2000);
-});
+  this.setCameraAngle = function(axis, angle) {
+    var data;
+    var axis_mask = (axis ? 1<<6 : 0);
+    var angle_mask = (angle & (1<<6)-1);
 
+    data = (1<<7) | axis_mask | angle_mask;
 
+    this.sp.write(data);
+  };  
 
+  this.getCameraAngle = function(axis) {
+    var data;
+    var axis_mask = (axis ? 1 : 0);
+    var read_mask = (1<<6);
 
+    data = 0 | read_mask | axis_mask;
 
+    sp.write(data);
+    sp.read(data);
 
+    var value = data & ((1<<7)-1);
+    return value;
+  };
 
+  this.setLed = function(value) {
+    var data;
+    var led_mask = (1<<5);
+    var switch_mask = (value ? 1 : 0);
+    data = 0 | led_mask | switch_mask;
 
+    sp.write(data);
+  };
 
+};
--- server.js
+++ server.js
@@ -10,6 +10,9 @@ var io = socketio(server);
 var Camera = require('./camera').Camera
 var camera = new Camera(0);
 
+var Serial = require('./serial').Serial
+var serial = new Serial('/dev/tty.usbmodem1421');
+
 app.use(express.static(__dirname + '/public'));
 
 server.listen(3000, function(){
@@ -29,6 +32,9 @@ io.of('/camera').on('connection', function(socket) {
   socket.on('move', function(data) {
       console.log(data.x);//[-1:1]
       console.log(data.y);
+
+      serial.setCameraAngle(1, 25);
+
   });
 
   socket.on('disconnect', function() {
